9a487aa5df9e1a97f9ffbd9e21b86ccddfea714e
Prativa98 <38049873+Prativa98@users.noreply.github.com>
2018-04-16 15:30:00 +0600

    Add files via upload

---
+++ array transfer.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+int a[3],b[3],i,j;
+ printf("enter the values for a[3]:");
+for(i=0;i<3;i++){
+    scanf("%d",&a[i]);}
+    printf("values of b[3]:\n");
+for (j=0;j<3;j++){
+b[j]=a[j];
+printf("%d\n",b[j]);
+}
+
+
+
+
+
+}
